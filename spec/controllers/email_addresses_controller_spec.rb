2330ea77438f4eba8fdf79b9cf5aad4b01b3e043
ZooBot Mars <sasa345@yahoo.com>
2015-11-01 16:40:24 -0700

    email address tests passing

--- spec/controllers/email_addresses_controller_spec.rb
+++ spec/controllers/email_addresses_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe EmailAddressesController, type: :controller do
   # EmailAddress. As you add validations to EmailAddress, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { email: "MyString", person_id: 1 }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { email: nil, person_id: nil }
   }
 
   # This should return the minimal set of values that should be in the session
@@ -102,15 +102,16 @@ RSpec.describe EmailAddressesController, type: :controller do
 
   describe "PUT #update" do
     context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+      let(:sara) { Person.create(first_name: 'Sara', last_name: 'Jones')}
+      let(:valid_attributes) { {email: 'somename@yahoo.com', person_id: sara.id}}
+      let(:new_attributes) { {email: 'MyNewString', person_id: sara.id}}
 
       it "updates the requested email_address" do
         email_address = EmailAddress.create! valid_attributes
         put :update, {:id => email_address.to_param, :email_address => new_attributes}, valid_session
         email_address.reload
-        skip("Add assertions for updated state")
+        expect(email_address.email).to eq('MyNewString')
+        expect(email_address.person_id).to eq(sara.id)
       end
 
       it "assigns the requested email_address as @email_address" do
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) {EmailAddress.new(email: "ladyG@yahoo.com", person_id: 1) }
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is invalid without an email address' do
+    email_address.email = nil
+    expect(email_address).not_to be_valid
+  end
+
+
+
+
+
+
+
+
+
 end
